da59f65e8973f4cbc13159d5ee4186169c90df8c
filosoferking <filosoferking@yahoo.com>
2015-07-24 09:18:00 -0700

    Calculator v1.4.1

--- calc.js
+++ calc.js
@@ -1,18 +1,3 @@
-/***************************************
- =============Version 0.1===============
- $("#add_button").click(function(){
-    var number0_val = $("#number_0").val();
-    var number1_val = $("#number_1").val();
-    add_numbers(number0_val, number1_val);
-});
-
- function add_numbers(operand1, operand2){
-    console.log('add numbers called');
-    var result = parseInt(operand1) + parseInt(operand2);
-    $("#result").val(result);
-}
- *****************************************/
-
 var operand_array = ["", ""];//array for my operand1 and operand2 input
 var index_pointer = 0;//determines which index position to push to
 var the_operator;//holds the operator that is clicked
@@ -25,20 +10,20 @@ function input_digit(operand) {//takes the input of the calculator buttons
     operand_array[index_pointer] += operand;//store the number clicked into the operand_array[0] if variable index_pointer = 0
 }
 
-function array_calc(){
-    if((operand_array[0] != "") && (operand_array[1] != "")){
-        calculate();
-        var array_result = $('#result_field').val();
-        var array_op = array_result + the_operator;
-        $('#input_field').val(array_op);
-        store_index_0()
+function array_calc(){//this runs the calculation if an operator is pushed
+    if((operand_array[0] != "") && (operand_array[1] != "")){//checks if both array index [0] and [1] to be a value to be calculated
+        calculate();//runs the calculate function
+        var array_result = $('#result_field').val();//stores the result of the calculation in a variable
+        var array_op = array_result + the_operator;//concatenates the selected operator to the result variable
+        $('#input_field').val(array_op);//inserts the concatenated result and operator into the input field
+        store_index_0();//stores the result into operand_array[0] and clears operand_array[1] for next selected number
 
     }
 }
 
-function store_index_0(){
-    operand_array[0] = $('#result_field').val();
-    operand_array[1] = "";
+function store_index_0(){//stores the result into operand_array[0] and clears operand_array[1] for next selected number
+    operand_array[0] = $('#result_field').val();//store the result into operand_array[0]
+    operand_array[1] = "";//clears operand_array[1];
 }
 
 function operator(op) {
@@ -47,89 +32,87 @@ function operator(op) {
     $('#input_field').val(operator_concat);//inserts concatenated value and operator back into display
     index_pointer = 1;//changes index_pointer = 1, the next number clicked will be inserted into operand_array[1]
     the_operator = op;//stores the operator clicked to be used in calculation comparison
-    //refresh();
     array_calc();
 }
 function add(operand1, operand2) {//adds operand1 and operand 2
     var add = parseFloat(operand1) + parseFloat(operand2);//parses the operands and stores them in the variable 'add
     $('#result_field').val(add);//inserts the calculated function into the display
-    store_index_0()
+    store_index_0();//stores the result into operand_array[0] and clears operand_array[1] for next selected number
 }
 
 function sub(operand1, operand2) {//subtracts operand1 and operand 2
     var sub = parseFloat(operand1) - parseFloat(operand2);//parses the operands and stores them in the variable 'sub
     $('#result_field').val(sub);//inserts the calculated function into the display
-    store_index_0()
+    store_index_0();//stores the result into operand_array[0] and clears operand_array[1] for next selected number
 }
 
 function mult(operand1, operand2) {//multiplies operand1 and operand 2
     var mult = parseFloat(operand1) * parseFloat(operand2);//parses the operands and stores them in the variable 'mult
     $('#result_field').val(mult);//inserts the calculated function into the display
-    console.log("Mult: ", mult)
-    store_index_0()
+    store_index_0();//stores the result into operand_array[0] and clears operand_array[1] for next selected number
 }
 
 function div(operand1, operand2) {//divides operand1 and operand 2
-    if (operand2 == 0) {
+    if (operand2 == 0) {//if operand2 equals the number 0
         $('#result_field').val("Undefined");//displays undefined if the number clicked was divided by 0
     } else {
         var div = parseFloat(operand1) / parseFloat(operand2);//parses the operands and stores them in the variable 'div
         $('#result_field').val(div);//inserts the calculated function into the display
     }
-    store_index_0()
+    store_index_0();//stores the result into operand_array[0] and clears operand_array[1] for next selected number
 }
-//NEED TO WRITE CODE TO MOVE RESULT INTO FIRST ARRAY INDEX[0], SIMILAR TO ARRAY_CALC
+
 function calculate() {//determines the operator to be used in and runs the function associated with that operator
     var num1 = operand_array[0];//stores the [0] position from the operand array into the num1 variable
     var num2 = operand_array[1];//stores the [1] position from the operand array into the num2 variable
     switch (the_operator) {//switch statement to determine the operator to use
-        case '+':
+        case '+'://if the_opertaotr variable is holding a '+'
             add(num1, num2);//runs the add() function
-            break;
-        case '-':
+            break;//exits switch statement if case is true
+        case '-'://if the_opertaotr variable is holding a '-'
             sub(num1, num2);//runs the sub() function
-            break;
-        case '*':
+            break;//exits switch statement if case is true
+        case '*'://if the_opertaotr variable is holding a '*'
             mult(num1, num2);//runs the mult() function
-            break;
-        case '/':
+            break;//exits switch statement if case is true
+        case '/'://if the_opertaotr variable is holding a '/'
             div(num1, num2);//runs the div() function
-            break;
+            break;//exits switch statement if case is true
     }
 }
 
-function all_clear() {
-    $('#input_field').val("");//puts an empty string back into the display
-    $('#result_field').val("");
-    console.log("Clear called");
-    operand_array[0] = "";
-    operand_array[1] = "";
-    index_pointer = 0;
-    first_op_click = true;
+function all_clear() {//clears all values and display
+    $('#input_field').val("");//inserts an empty string back into the input field
+    $('#result_field').val("");//inserts and empty string back into the result field
+    operand_array[0] = "";//inserts an empty string back into the operand_array[0]
+    operand_array[1] = "";//inserts an empty string back into the operand_array[1]
+    index_pointer = 0;//sets the next input_digit() to go into operand_array[0]
+    first_op_click = true;//boolean value to declare that an operator has not yet been clicked to do a calculation
 }
-$('#all_clear').click(function () {//clears the display
-    all_clear();
+$('#all_clear').click(function () {//click function to run all_clear()
+    all_clear();//calls all_clear() function
 })
 
-$('#clear').click(function () {
-    clear();
-    refresh();
+$('#clear').click(function () {//click function to run clear() and refresh()
+    clear();//clears the result field and resets operand_array[1]
+    refresh_display();//inserts current operand_array values and operator into input field
 })
 
-function invert() {
-    var number = $('#input_field').val();
-    var inversion = "-" + number;
-    $('#input_field').val(inversion);
+function clear() {//clears the result field and resets operand_array[1]
+    $('#result_field').val("");//inserts an empty string back into the display
+    operand_array[1] = "";//inserts an empty string back into the operand_array[1]
 }
 
-function clear() {
-    $('#result_field').val("");//puts an empty string back into the display
-    operand_array[1] = "";
+function refresh_display() {//inserts current array and operator into input field
+    var output = operand_array[0] + the_operator + operand_array[1];//holds the values of operand_array[0], the operator, and operand_array[1] in a variable
+    $('#input_field').val(output);//inserts the output variable into the input field
 }
 
-function refresh() {
-    var output = operand_array[0] + the_operator + operand_array[1];
-    $('#input_field').val(output);
+function invert() {//inverts pos or neg sign to result
+    var number = operand_array[0];//stores value of operand_array[0] into variable
+    var inversion =  number * -1;//multiplies number variable by -1 and stores it on inversion variable
+    operand_array[0] = inversion;//inserts inversion variable into operand_array[0]
+    $('#input_field').val(inversion);//inserts inversion variable value into input field
 }
 
 
